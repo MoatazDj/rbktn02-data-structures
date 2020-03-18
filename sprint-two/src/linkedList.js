a16fb08bc24e4bcc92a39ae492e9cab28b283652
MootezDj <djebalimootez1@gmail.com>
2020-03-18 14:13:06 +0100

    data Structures sprint 2

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,14 +4,32 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value)
+    if(this.head === null){
+        this.head=newNode
+        this.tail=newNode
+    }
+    this.tail.next = newNode
+    this.tail= newNode
+    return this.tail
   };
 
   list.removeHead = function() {
+    var removedHead = this.head.value
+    this.head = this.head.next
+    return removedHead
   };
 
   list.contains = function(target) {
+      var searchTarget = this.head
+      while(searchTarget !== null){
+          if(searchTarget.value === target){
+            return true
+          }
+          searchTarget = searchTarget.next
+      }
+      return false
   };
-
   return list;
 };
 
